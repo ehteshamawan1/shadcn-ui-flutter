ec49b49e18bcebc3efaae4e85c34d767abfa0092
Ehtesham Awan <ehtesham.awan.ea@gmail.com>
2025-12-07 22:26:17 +0500

    NFC foreground popup issue solved

--- android/app/src/main/kotlin/com/skadan/ska_dan_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/skadan/ska_dan_flutter/MainActivity.kt
@@ -1,5 +1,22 @@
 package com.skadan.ska_dan_flutter
 
 import io.flutter.embedding.android.FlutterActivity
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        val adapter: NfcAdapter? = NfcAdapter.getDefaultAdapter(this)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_IMMUTABLE)
+        adapter?.enableForegroundDispatch(this, pendingIntent, null, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val adapter: NfcAdapter? = NfcAdapter.getDefaultAdapter(this)
+        adapter?.disableForegroundDispatch(this)
+    }
+}
